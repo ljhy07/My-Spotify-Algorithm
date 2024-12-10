29a9499d5fcd522c645c647ab2b090e1602c46e5
이재현 <2023050@bssm.hs.kr>
2024-12-10 10:07:31 +0900

    프론트 일부 세팅&토큰 저장 기능 추가

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI, Request, HTTPException
+from fastapi import FastAPI, Response, Request, HTTPException, Cookie
 from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
 import random
 import string
+import json
+import jwt
 import httpx
 import base64
 from urllib.parse import urlencode
@@ -14,13 +17,26 @@ env = load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 app = FastAPI()
 
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
+
 def generate_random_string(length: int) -> str:
-    """Generates a random string of the specified length."""
     return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
 
 @app.get("/")
 def main():
-    return "hello"
+    
+
+    return 
 
 @app.get("/login")
 async def Login():
@@ -40,7 +56,7 @@ async def Login():
     return RedirectResponse(url=spotify_auth_url)
 
 @app.get("/login/callback")
-async def LoginCallback(request: Request):
+async def LoginCallback(response: Response, request: Request):
     code = request.query_params.get("code")
     state = request.query_params.get("state")
     
@@ -64,10 +80,67 @@ async def LoginCallback(request: Request):
     }
 
     async with httpx.AsyncClient() as client:
-        response = await client.post(auth_url, headers=auth_headers, data=auth_data)
-        if response.status_code != 200:
+        result = await client.post(auth_url, headers=auth_headers, data=auth_data)
+        if result.status_code != 200:
             print(f"Response: {response.status_code}, Body: {response.text}")
             raise HTTPException(status_code=response.status_code, detail="Failed to fetch token")
 
-        token_data = response.json()
-        return token_data
+        token_data = json.loads(result.json())
+
+        response.set_cookie(
+            key="access_token",
+            value=token_data["access_token"],
+            httponly=True,    # JavaScript에서 접근 불가
+            secure=True,      # HTTPS에서만 사용
+            samesite="Strict", # CSRF 방지
+            max_age=token_data["expires_in"],     # 1시간
+        )
+
+        response.set_cookie(
+            key="refresh_token",
+            value=token_data["refresh_token"],
+            httponly=True,    # JavaScript에서 접근 불가
+            secure=True,      # HTTPS에서만 사용
+            samesite="Strict", # CSRF 방지
+            max_age=604800, # 7일
+        )
+
+@app.post("/refresh")
+async def Refresh(response: Response, refresh_token: str = Cookie(None)):
+    client_id = os.environ["CLIENT_ID"]
+    client_secret = os.environ["CLIENT_SECRET"]
+    auth_url = "https://accounts.spotify.com/api/token"
+    auth_headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Authorization": "Basic " + (base64.b64encode(f"{client_id}:{client_secret}".encode())).decode()
+    }
+    auth_data = {
+        "grant_type": "refresh_token",
+        "refresh_token": refresh_token
+    }
+
+    async with httpx.AsyncClient() as client:
+        result = await client.post(auth_url, headers=auth_headers, data=auth_data)
+        if result.status_code != 200:
+            print(f"Response: {response.status_code}, Body: {response.text}")
+            raise HTTPException(status_code=response.status_code, detail="Failed to fetch token")
+
+        token_data = json.loads(result.json())
+
+        response.set_cookie(
+            key="access_token",
+            value=token_data["access_token"],
+            httponly=True,    # JavaScript에서 접근 불가
+            secure=True,      # HTTPS에서만 사용
+            samesite="Strict", # CSRF 방지
+            max_age=token_data["expires_in"],     # 1시간
+        )
+
+        response.set_cookie(
+            key="refresh_token",
+            value=token_data["refresh_token"],
+            httponly=True,    # JavaScript에서 접근 불가
+            secure=True,      # HTTPS에서만 사용
+            samesite="Strict", # CSRF 방지
+            max_age=604800, # 7일
+        )
